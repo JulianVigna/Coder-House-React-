ebf00be6b92bf83255ee9382ef0f6f0bcb061085
julian vigna <juuliaan_@hotmail.com>
2022-12-21 14:29:56 -0300

    primer commit para entrega

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Saludo from './components/ItemListContainer/ItemListContainer';
-import Logo from './components/Logo/Logo';
+
 function App() {
   return (
     <div >
-     
-     
       <NavBar/> 
       
       <Saludo/>
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,4 +1,3 @@
-import "./logo.css"
 
 function Logo () {
     return (
