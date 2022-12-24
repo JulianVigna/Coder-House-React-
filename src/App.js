5585e2e1dd1978d33db1b80500a084bf43921fda
julian vigna <juuliaan_@hotmail.com>
2022-12-24 16:43:19 -0300

    primera entrega

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-import Saludo from './components/ItemListContainer/ItemListContainer';
+import Saludo from './components/ItemListContainer/greetings';
 
 function App() {
   return (
     <div >
       <NavBar/> 
       
-      <Saludo/>
+      <Saludo
+      title="Bienvenido a Electro Mundo"
+      name="Mi nombre es Julian Vigna"
+      description="El proyecto se trata de un e-comerce de todos equipos electronicos."/>
 
     </div>
   );
---
+++ src/components/ItemListContainer/greetings.jsx
@@ -0,0 +1,13 @@
+import "./greetings"
+
+function Saludo (props){
+    return(
+        <div >
+            <h1 className="saludoIncial ">{props.title}</h1>
+            <p>{props.name}</p>
+            <p>{props.description}</p>
+        </div>
+    )
+}
+
+export default Saludo;
